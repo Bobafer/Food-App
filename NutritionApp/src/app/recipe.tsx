5f0fdbbf61320d15493bdca564675ff04860eca0
Ruhan Dutta <ruhan122011@gmail.com>
2026-06-15 17:46:53 -0400

    Pizza image for recipe

--- NutritionApp/src/app/recipe.tsx
+++ NutritionApp/src/app/recipe.tsx
@@ -1,8 +1,12 @@
-import {View, Text} from 'react-native'
+import {View, Text, Image} from 'react-native'
+import pizza from '@/assets/Recipe_Images/pizza.jpg'
 
 export const Recipe = () => {
     return(
         <View> 
+            <Image
+            source ={pizza}>
+            </Image>
             <Text>Recipes</Text>
         </View>
     )
