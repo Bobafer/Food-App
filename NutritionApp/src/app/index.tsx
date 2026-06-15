fb95ef203fea30b987e090ceb06e712de9a1b5ec
Ruhan Dutta <ruhan122011@gmail.com>
2026-06-15 17:27:26 -0400

    Add the text Recipe
    Created the recipe view

--- NutritionApp/src/app/index.tsx
+++ NutritionApp/src/app/index.tsx
@@ -1,9 +1,10 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, } from "react-native";
+import {Recipe} from './recipe'
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <Recipe></Recipe>
     </View>
   );
 }
