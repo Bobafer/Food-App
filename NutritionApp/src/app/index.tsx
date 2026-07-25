5229834e451da1983005de8bc8331f8c51dec2f9
Anay-Desai <anaydesai2011@gmail.com>
2026-07-25 12:51:32 -0400

    NavBar

--- NutritionApp/src/app/index.tsx
+++ NutritionApp/src/app/index.tsx
@@ -1,12 +1,17 @@
 import { Text, View, StyleSheet, } from "react-native";
 import {Recipe} from './recipe'
 import {Portion} from './portion'
+import { Button } from "expo-router/build/react-navigation";
+import {NavBar} from './NavBar'
+
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Recipe></Recipe>
-      
+      {/* <Recipe></Recipe> */}
+      <NavBar>
+        
+      </NavBar>
     </View>
   );
 }
@@ -14,7 +19,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
+    alignItems: "stretch",
     justifyContent: "center",
   },
 });
